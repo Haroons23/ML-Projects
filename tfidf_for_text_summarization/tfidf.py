1bcbdc236e2f61cba65b42caf7c1e330514fba4c
Haroon Salimi <hsalimi@uoguelph.ca>
2020-05-05 02:10:16 -0400

    Fixed indentation problems.

--- tfidf_for_text_summarization/tfidf.py
+++ tfidf_for_text_summarization/tfidf.py
@@ -39,8 +39,8 @@ def prep_documents(documents):
         # lemmatizing, removing stopwords.
         updated_doc = []
         for word in words:
-        	if word not in stop_words:
-        		updated_doc.append(lem.lemmatize(word))
+            if word not in stop_words:
+                updated_doc.append(lem.lemmatize(word))
 
         # Adding updated document to list.
         prepped_documents.append(updated_doc)
@@ -58,24 +58,24 @@ def get_word_frequency(documents):
     for doc_num in range(len(documents)):
 
         # Go through each word in document.
-    	for word in documents[doc_num]:
+        for word in documents[doc_num]:
 
-    		# Word has appeared but its the first time in this document.
-    		if word in frequency and len(frequency[word]) == doc_num:
-    			frequency[word].append(documents[doc_num].count(word))
+            # Word has appeared but its the first time in this document.
+            if word in frequency and len(frequency[word]) == doc_num:
+                frequency[word].append(documents[doc_num].count(word))
 
-    		# Word hasn't appeared yet. Create array pad previous doc values with zero. 
-    		elif word not in frequency:
-    			frequency[word] = []
-    			for i in range(doc_num):
-    				frequency[word].append(0)
+            # Word hasn't appeared yet. Create array pad previous doc values with zero. 
+            elif word not in frequency:
+                frequency[word] = []
+                for i in range(doc_num):
+                    frequency[word].append(0)
 
-    			frequency[word].append(documents[doc_num].count(word))
+                frequency[word].append(documents[doc_num].count(word))
 
-    	# Add a zero to all the word counts that didn't appear in the current document. 
-    	for word in frequency:
-    		if len(frequency[word]) < doc_num + 1:
-    			frequency[word].append(0)
+        # Add a zero to all the word counts that didn't appear in the current document. 
+        for word in frequency:
+            if len(frequency[word]) < doc_num + 1:
+                frequency[word].append(0)
 
     return frequency
 
@@ -83,12 +83,12 @@ def get_word_frequency(documents):
 # Returns the number of words in each document, in array format where index zero is represents first document. 
 def get_document_sizes(documents):
 
-	sizes = []
+    sizes = []
 
-	for doc in documents:
-		sizes.append(len(doc))
+    for doc in documents:
+        sizes.append(len(doc))
 
-	return sizes
+    return sizes
 
 
 # Returns the number of documents each word appears in.
@@ -101,12 +101,12 @@ def get_num_documents_per_word(word_frequency):
     for word in word_frequency:
 
         # Iterate through each document and tally the results.
-    	num_docs_per_word[word] = 0
-    	for doc in word_frequency[word]:
+        num_docs_per_word[word] = 0
+        for doc in word_frequency[word]:
 
-    		# If the word appears in the document, increment count.
-    		if doc > 0:
-    			num_docs_per_word[word] += 1
+            # If the word appears in the document, increment count.
+            if doc > 0:
+                num_docs_per_word[word] += 1
 
     return num_docs_per_word
 
@@ -115,66 +115,66 @@ def get_num_documents_per_word(word_frequency):
 def calculate_tfidf(word_frequency, documents):
 
     # How many documents each word appears in and how many words in each document. 
-	num_docs_per_word = get_num_documents_per_word(word_frequency)
-	document_sizes = get_document_sizes(documents)
+    num_docs_per_word = get_num_documents_per_word(word_frequency)
+    document_sizes = get_document_sizes(documents)
 
-	# Calculating TF.
-	tf = {}
-	for word in word_frequency:
-		tf[word] = np.array(word_frequency[word]) / np.array(document_sizes)
+    # Calculating TF.
+    tf = {}
+    for word in word_frequency:
+        tf[word] = np.array(word_frequency[word]) / np.array(document_sizes)
 
-	# Calculating IDF.
-	idf = {}
-	for word in word_frequency:
-		idf[word] = np.log( len(document_sizes) / num_docs_per_word[word])
+    # Calculating IDF.
+    idf = {}
+    for word in word_frequency:
+        idf[word] = np.log( len(document_sizes) / num_docs_per_word[word])
 
-	# Calculating TD-IDF.
-	tfidf = {}
-	for word in word_frequency:
-		tfidf[word] = tf[word] * idf[word]
+    # Calculating TD-IDF.
+    tfidf = {}
+    for word in word_frequency:
+        tfidf[word] = tf[word] * idf[word]
 
-	return tfidf
+    return tfidf
 
 
 # Calculates Document Rank ie finding the most important documents by summing TFIDF scores of their words. 
 def calculate_document_rank(tfidf, documents):
 
-	scores = [0] * len(documents)
+    scores = [0] * len(documents)
 
     # Iterating through each document. 
-	for i in range(len(documents)):
+    for i in range(len(documents)):
 
-		# Summing tfidf scores for individual words therefore representing the document. 
-		for word in documents[i]:
-			scores[i] += tfidf[word][i]
+        # Summing tfidf scores for individual words therefore representing the document. 
+        for word in documents[i]:
+            scores[i] += tfidf[word][i]
 
-	# Sorting documents based off score.
-	ranked = [x for _, x in sorted(zip(scores, documents), reverse=True)]
-	scores.sort(reverse=True)
+    # Sorting documents based off score.
+    ranked = [x for _, x in sorted(zip(scores, documents), reverse=True)]
+    scores.sort(reverse=True)
 
-	for i in range(len(scores)):
-		print(f'Document Score: {scores[i]}. Document: {ranked[i]}')
+    for i in range(len(scores)):
+        print(f'Document Score: {scores[i]}. Document: {ranked[i]}')
 
 
 def main():
 
-	# Six documents to analyze.
-	d1 = "born December 30, 1984 is an American professional basketball player for the Los Angeles Lakers of the National Basketball Association (NBA)."
-	d2 = "He is widely considered to be one of the greatest basketball players in NBA history, and discussions ranking him as the greatest basketball player of all time have often been subject to significant debate, with frequent comparisons to Michael Jordan.[1]"
-	d3 = "James's teams have played in eight consecutive NBA Finals (2011–2018 seasons) between the Miami Heat and Cleveland Cavaliers."
-	d4 = "His accomplishments include three NBA championships, four NBA Most Valuable Player (MVP) Awards, three Finals MVP Awards, and two Olympic gold medals." 
-	d5 = "James holds the all-time record for playoffs points, is third in all-time points, and eighth in all-time assists." 
-	d6 = "James was selected to the All-NBA First Team twelve times (all-time record), made he All-Defensive First Team five times, and has played in sixteen All-Star Games, in which he was selected All-Star MVP three times."
+    # Six documents to analyze.
+    d1 = "born December 30, 1984 is an American professional basketball player for the Los Angeles Lakers of the National Basketball Association (NBA)."
+    d2 = "He is widely considered to be one of the greatest basketball players in NBA history, and discussions ranking him as the greatest basketball player of all time have often been subject to significant debate, with frequent comparisons to Michael Jordan.[1]"
+    d3 = "James's teams have played in eight consecutive NBA Finals (2011–2018 seasons) between the Miami Heat and Cleveland Cavaliers."
+    d4 = "His accomplishments include three NBA championships, four NBA Most Valuable Player (MVP) Awards, three Finals MVP Awards, and two Olympic gold medals." 
+    d5 = "James holds the all-time record for playoffs points, is third in all-time points, and eighth in all-time assists." 
+    d6 = "James was selected to the All-NBA First Team twelve times (all-time record), made he All-Defensive First Team five times, and has played in sixteen All-Star Games, in which he was selected All-Star MVP three times."
 	
-	# Cleans documents. 
-	documents = prep_documents([d1, d2, d3, d4, d5, d6])
+    # Cleans documents. 
+    documents = prep_documents([d1, d2, d3, d4, d5, d6])
 
-	# Calculating TFIDF.	
-	word_frequency = get_word_frequency(documents)
-	tfidf = calculate_tfidf(word_frequency, documents)
+    # Calculating TFIDF.	
+    word_frequency = get_word_frequency(documents)
+    tfidf = calculate_tfidf(word_frequency, documents)
 
-	# Ranking documents in importance.
-	calculate_document_rank(tfidf, documents)
+    # Ranking documents in importance.
+    calculate_document_rank(tfidf, documents)
 
 
 if __name__ == "__main__":
